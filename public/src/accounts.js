e3afecbb6417942c9c9f4d7f05c71fe196fdb311
thinkfuljerry <Jerry.Iturralde.2014@gmail.com>
2023-10-27 14:41:59 -0700

    Complete Qualified Assessment

---
+++ public/src/accounts.js
@@ -0,0 +1,47 @@
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+}
+
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((accountA, accountB) => {
+    const lastNameA = accountA.name.last.toLowerCase();
+    const lastNameB = accountB.name.last.toLowerCase();
+
+    return lastNameA.localeCompare(lastNameB);
+  });
+}
+
+function getAccountFullNames(accounts) {
+  return accounts.map(account => `${account.name.first} ${account.name.last}`);
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
